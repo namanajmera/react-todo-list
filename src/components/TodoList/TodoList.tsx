43f61d89306e79025bff1b7e1cabe27462487c3c
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-09-22 16:07:50 +0530

    Added: Show the Todo dynamically and handle checkbox cases.

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -10,7 +10,7 @@ const TodoList = (props: Props) => {
     const [todoList, setTodoList] = useState<todoList[]>([
         {
             id: 1,
-            name: "Testing",
+            name: "TestingTesting Testing Testing Testing TestingTesting Testing   TestingTestingTestingTestingTesting  Testing Testing Testing TestingTesting Testing Testing Testing",
             isCompleted: false,
         },
         {
@@ -35,16 +35,30 @@ const TodoList = (props: Props) => {
         },
     ]);
 
+    const checkIsCompleted = () => {
+        const checkCompleted = todoList && todoList.filter((todo: todoList, index) => {
+            return todo?.isCompleted === true;
+        })
+        return checkCompleted.length;
+    }
+
+    const handleSelect = (id: number) => {
+        const updatedTodo: todoList[] = todoList.map((todo: todoList) =>
+            id === todo?.id ? { ...todo, isCompleted: !todo.isCompleted } : { ...todo }
+        );
+        setTodoList(updatedTodo);
+    }
+
     return (
         <div className={style["todo-list-container"]}>
             <div className={style["info"]}>
                 <div className={style["task-created"]}>
                     <span className={style["title"]}>Task Created</span>
-                    <span className={style["count"]}>0</span>
+                    <span className={style["count"]}>{todoList.length}</span>
                 </div>
                 <div className={style["completed"]}>
                     <span className={style["title"]}>Completed</span>
-                    <span className={style["total-count"]}>0 out of 0</span>
+                    <span className={style["total-count"]}>{checkIsCompleted()} out of {todoList.length}</span>
                 </div>
             </div>
             <div className={style["list-container"]}>
@@ -54,12 +68,11 @@ const TodoList = (props: Props) => {
                     <span className={style["empty-text-2"]}>Create tasks and organize your to-do items</span>
                 </div> :
                     <div className={style["tasks-container"]}>
-                        <TodoListItems />
-                        <TodoListItems />
-                        <TodoListItems />
-                        <TodoListItems />
-                        <TodoListItems />
-                        <TodoListItems />
+                        {
+                            todoList && todoList.map((todo, index) => (
+                                <TodoListItems key={todo.id} name={todo.name} isCompleted={todo.isCompleted} id={todo.id} changeValue={handleSelect} />
+                            ))
+                        }
                     </div>}
             </div>
         </div>
--- src/components/TodoListItems/TodoListItems.tsx
+++ src/components/TodoListItems/TodoListItems.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import style from "./TodoListItems.module.less";
 import { Trash } from '../../assets/images';
 
-type Props = {}
+type Props = {
+    id: number
+    name: String,
+    isCompleted: boolean
+    changeValue: any
+}
 
-const TodoListItems = (props: Props) => {
+const TodoListItems = ({ name, isCompleted, changeValue, id }: Props) => {
     return (
         <div className={style['task-container']}>
-            <input className={style['checked']} type="checkbox" name="" id="" />
-            <span className={style['text']}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores quasi ipsum tenetur nam exercitationem nesciunt minus.</span>
+            <input className={style['checked']} type="checkbox" name="" id="" checked={isCompleted}
+                onChange={() => changeValue(id)} />
+            <span className={`${isCompleted && style['done-task']} ${style['text']}`}>{name}</span>
             <img className={style['trash-icon']} src={Trash} alt="" />
         </div>
     )
